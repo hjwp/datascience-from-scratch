aeef365ee184a34a6cc5e9f4b15df77df3bc3605
Harry Percival <percival@arrival.com>
2022-02-28 11:27:51 +0000

    week 4 linear algebra

---
+++ matrices.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def test_matrix_addition():
+    matrix1 = np.array([
+        [1, 2, 3],
+        [4, 5, 6]
+    ])
+    matrix2 = np.array([
+        [10, 11, 12],
+        [13, 14, 15],
+    ])
+
+    result = matrix1 + matrix2
+
+    print("result", result)
+
+    expected = np.array([
+        [11, 13, 15],
+        [17, 19, 21],
+    ])
+    print("expected", expected)
+
+    assert np.equal(result, expected)
---
+++ vectors.py
@@ -0,0 +1,58 @@
+from typing import List, Union
+
+
+x = 3  # type: int
+y = "hello"  # type: str
+things = [1, 2, 3]  # type: list
+
+
+Vector = List[Union[float, int]]
+
+assert 1 + 1 == 2
+assert True
+
+
+def add(v1: Vector, v2: Vector) -> Vector:
+    assert len(v1) == len(v2), "vectors not same length"
+    return [v1[i] + v2[i] for i in range(len(v1))]
+
+
+def subtract(v1: Vector, v2: Vector) -> Vector:
+    assert len(v1) == len(v2), "vectors not same length"
+    return [v1[i] - v2[i] for i in range(len(v1))]
+
+
+def sum_of_ith_elements(vectors: List[Vector], i) -> float:
+    return sum([v[i] for v in vectors])
+
+
+def vector_sum(vectors: List[Vector]) -> Vector:
+    assert len(vectors) > 0, "need at least one vector"
+    vector_length = len(vectors[0])
+
+    return [
+        sum_of_ith_elements(vectors, i) 
+        for i in range(vector_length)
+    ]
+
+
+def test_add():
+    v1 = [1, 2]
+    v2 = [2, 3]
+    assert add(v1, v2) == [3, 5]
+    assert add([1, 2, 3], [4, 5, 7]) == [5, 7, 10]
+
+
+def test_subtract():
+    assert subtract([4, 5, 7], [1, 2, 3]) == [3, 3, 4]
+
+
+
+def test_sum():
+    v1 = [4, 5, 7]
+    v2 = [1, 2, 3]
+    v3 = [10, 11, 12]
+    vectors = [v1, v2, v3]
+    assert vector_sum(vectors) == [15, 18, 22]
+    vectors = [v1, v2]
+    assert vector_sum(vectors) == [5, 7, 10]
