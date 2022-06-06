5feaa5f14cde3516e7b947a516d025a8eae4f067
Harry Percival <percival@arrival.com>
2022-06-06 22:33:54 +0100

    add explanatory comments

--- bayes.py
+++ bayes.py
@@ -36,20 +36,39 @@ class NaiveBayesClassifier:
                 self.total_ham_messages += 1
 
     def predict(self, text: str) -> float:
-        message_tokens = text.split()
+        # (my attempted rewording of) Bayes' theorem: 
+        # the probability of some outcome given some known fact,
+        # equals the probability of the fact given the outcome,
+        # divided by the "overall" probability of the fact (?)
+        # (ie the probability of the fact given the outcome plus the probability of the fact given not-the-outcome)
+        # P(A|B) = P(B|A) / [ P(B|A) + P(B|~A) ]
+        # 
+        # ie, probability that message is spam given these tokens,
+        # equals probability of seeing these tokens given that it is spam,
+        # divided by the probability of seeing these tokens given that it is spam
+        # plus given that it is not spam.
+        # P(S|T) = P(T|S) / [ P(T|S) + P(T|~S) ]
 
+        tokens_seen = text.split()
+        # we compute the probability of seeing all these tokens under both assumptions,
+        # by multiplying together the probabilities of each individual token.
+
+        # 1 is the right "null" (identity) value for multiplying together probabilities
         p_all_tokens_given_spam = 1.0
         p_all_tokens_given_ham = 1.0
 
         for token in self.tokens:
-            if token in message_tokens:
+            if token in tokens_seen:
+                # assuming it is spam, what is the probability that we should have seen this token?
                 p_all_tokens_given_spam *= self.p_token_given_spam(token)
+                # assuming it is not spam, what is the probability that we should have seen this token?
                 p_all_tokens_given_ham *= self.p_token_given_ham(token)
             else:
+                # assuming it is spam, what is the probability that we should *not* have seen this token?
                 p_all_tokens_given_spam *= (1 - self.p_token_given_spam(token))
+                # assuming it is not spam, what is the probability that we should *not* have seen this token?
                 p_all_tokens_given_ham *= (1 - self.p_token_given_ham(token))
 
-        # the return p(spam) / p(spam) + p(ham)
         return p_all_tokens_given_spam / (p_all_tokens_given_spam + p_all_tokens_given_ham)
 
 
